681e16076c8d9121c8bf0d3d86a4beb03124f313
esasova <sassova.elena@gmail.com>
2020-12-14 15:45:16 +0100

    api verification

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const film = document.getElementById('film');
-const getFilm = async () => {
-    const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=7cfb6335');
-    const responseJson = await response.json();
-    console.log(responseJson)
 
+const getFilm = new XMLHttpRequest();
+getFilm.onreadystatechange = function(){
+    if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        let response = JSON.parse(this.responseText);
+        film.textContent = response;
+    }
 }
+getFilm.open('GET', 'http://www.omdbapi.com/?i=tt3896198&apikey=7cfb6335');
+getFilm.send();
+
+// const getFilm = async () => {
+//     const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=7cfb6335');
+//     const responseJson = await response.json();
+//     console.log(responseJson)
+
+// }
