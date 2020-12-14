3ff488507dfa66a45c120e01a4c3c1d67be26b88
esasova <sassova.elena@gmail.com>
2020-12-14 15:52:29 +0100

    non async method +

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const getFilm = new XMLHttpRequest();
 getFilm.onreadystatechange = function(){
     if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         let response = JSON.parse(this.responseText);
-        film.textContent = response;
+        console.log(response.Title)
+        const film = document.getElementById('film');
+        film.textContent = response.Title;
     }
 }
 getFilm.open('GET', 'http://www.omdbapi.com/?i=tt3896198&apikey=7cfb6335');
