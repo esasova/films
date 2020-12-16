420c60a9434c07b534a4f847da72c8b646fe0af5
esasova <sassova.elena@gmail.com>
2020-12-16 14:21:52 +0100

    affichage sur la page

--- index.js
+++ index.js
@@ -3,29 +3,56 @@ const input = document.getElementById('query');
 
 // bouton de recherche
 const search = document.getElementById('search');
+
 // l'url de base de l'API
 const url = 'https://imdb-api.com/en/API/SearchMovie/k_7wu03o0q/';
+
+
 
 // la fonction qui récupère les données correspondant à la recherche
 async function getFilm(){
     const query = input.value;
     const endpoint = `${url}${query}`;
-    console.log(endpoint);
     try{
         const request = await fetch(endpoint);
         const result = await request.json();
-        console.log(result)
+        affichage(result.results)
+
+
     }
     catch (error){
     console.log(error)
     }
 }
-// //fonction qui montre les résultats
-// function displayFilm() {
-    
-//     let essai = getFilm();
-//     console.log(essai);
-// }
 
 //resultats 
-search.addEventListener('click', getFilm)
+search.addEventListener('click', getFilm);
+
+//la section des résultats
+const section = document.getElementById('choice');
+
+
+//la fonction qui itère l'array des résultats
+const affichage = (tableau) =>{
+    for(objet of tableau){
+        const article = document.createElement('article');
+        section.appendChild(article);
+        article.classList.add('col-12 col-md-6 col-4')
+        for(element in objet) {
+            if(element === 'title'){
+                let titre = document.createElement('h3');
+                titre.textContent = `${objet[element]}`;
+                article.appendChild(titre)
+            } else if(element === 'image'){
+                let image = document.createElement('p');
+                image.innerHTML += `<img src="${objet[element]}"></img>`;
+                article.appendChild(image) 
+                
+            } else if(element === 'description') {
+                let year = document.createElement('p');
+                year.textContent = `Year : ${objet[element]}`;
+                article.appendChild(year)
+            }
+        }
+    }
+}
